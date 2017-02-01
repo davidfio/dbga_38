422b36a4ee59cef70c5f9c5dd8251bd3e7d23f67
dbga_38 <dbga_38@dbga.local>
2017-02-01 17:33:44 +0100

    - Start the Finite-State-Machine

---
+++ DBGA_38 Exercise/Assets/FSM/FSMState.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum Transition
+{
+    NullTransition = 0,
+}
+
+public enum StateID
+{
+    NullStateID = 0,
+}
+
+public abstract class FSMState : MonoBehaviour
+{
+    
+    protected Dictionary<Transition, StateID> map = new Dictionary<Transition, StateID>();
+    protected StateID stateID;
+
+    public StateID ID {get { return stateID; } }
+
+    public void AddTransition(Transition _trans, StateID _id)
+    {
+        map.Add(_trans, _id);
+    }
+
+    public void DeleteTransition(Transition _trans)
+    {
+        if (!map.ContainsKey(_trans))
+        {
+            Debug.LogError(_trans + " isn't on the tranistion's list");
+            return;
+        }
+        map.Remove(_trans);
+    }
+
+    public StateID GetOutPutState(Transition _trans)
+    {
+        return map.ContainsKey(_trans) ? map[_trans] : StateID.NullStateID;
+    }
+}
+
+public class FSMSystem
+{
+    private List<FSMState> statesList;
+
+    // Use the constructor for initialise because isn't MonoBehaviour
+    public FSMSystem()
+    {
+        statesList = new List<FSMState>();
+    }
+
+    // The way to change the state of the FSM is to perform a transition
+    // NEVER EDIT DIRECTLY CURRENTSTATE
+    private StateID _currenStateID;
+    public StateID CurretStateID { get { return _currenStateID; } }
+    private FSMState _currentState;
+    public FSMState CurrentState { get { return _currentState; } }
+}
